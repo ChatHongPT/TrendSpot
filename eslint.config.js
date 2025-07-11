9aeeb6912d8827e79e1b8132f2f186ecd7b7df41
ChatHongPT <suk924600@naver.com>
2025-07-12 08:21:32 +0900

    🔨fix: 라우팅 문제 React Router에서 새로고침 시 404 발생 문제 해결

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import tseslint from "typescript-eslint";
+import js from "@eslint/js";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json", // 정확한 tsconfig 경로
+        sourceType: "module",
+      },
     },
   },
-])
+];
